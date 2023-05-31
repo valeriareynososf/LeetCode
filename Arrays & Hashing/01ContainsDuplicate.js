96e0923ec033af5f815b5177dd5753c18bcd6985
valeriareynososf <valeriayearup@gmail.com>
2023-05-31 15:46:44 -0700

    added arrays

---
+++ Arrays & Hashing/01ContainsDuplicate.js
@@ -0,0 +1,35 @@
+{/*
+217. Contains Duplicate
+Easy
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+*/}
+
+const containsDuplicate = function(nums) {
+const obj = {};
+    for (const num of nums) {
+        if (!(num in obj)) {
+            obj[num] = 1
+        } else {
+            obj[num]++
+            let val = obj[num]
+            if (val > 1) return true;
+        }
+    }
+    return false;
+};
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
